7848879b0a02f6820e18930f33152429bc1eb796
Jennifer <jenn26@iastate.edu>
2021-03-03 14:21:17 -0600

    Changed health

--- Assets/GlobalVariables.cs
+++ Assets/GlobalVariables.cs
@@ -8,7 +8,7 @@ public class GlobalVariables : MonoBehaviour
     public static class globalvars
     {
         public static int moneyAmount = 0;
-        public static int playerHealth = 100;
+        public static int playerHealth = 10;
         public static int playerStrength = 100;
         public static int armorLevel = 1;
         public static int weaponLevel = 1;
@@ -27,12 +27,12 @@ public class GlobalVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
