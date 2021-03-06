df4a34cc80b281604e7a2639e78d9a7d5a40fc26
tjtln <tjtln@iastate.edu>
2021-03-05 22:53:28 -0600

    added a main menu button

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void clickedOnButton()
+    {
+        //replace with whatever takes you to the main menu
+        Debug.Log("Replace this code in the Main Menu Script");
+    }
+    }
