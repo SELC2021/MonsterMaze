ca2d7e2599a72492d2a39230ba4043d5f5b68d8a
tjtln <tjtln@iastate.edu>
2021-03-05 23:05:11 -0600

    very basic main menu

---
+++ Assets/Level1button.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1button : MonoBehaviour
+{
+    public void clickedOnButton()
+    {
+        Debug.Log("Replace with however you get to Scene 1");
+    }
+}
