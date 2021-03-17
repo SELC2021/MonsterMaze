90833c69713dc9b7579696723ea1b7eac621a262
tjtln <tjtln@iastate.edu>
2021-03-17 15:11:21 -0500

    redid maze 1

--- Assets/Scripts/enemy1.cs
+++ Assets/Scripts/enemy1.cs
@@ -10,14 +10,14 @@ public class enemy1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float distance = Vector3.Distance(GameObject.FindWithTag("Player").transform.position, transform.position);
-        if (distance < 10f)
+        if (distance < 8f)
         {
             position = gameObject.transform.position;
             target = GameObject.Find("Head").transform.position;
