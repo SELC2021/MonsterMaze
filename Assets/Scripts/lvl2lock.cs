4de79a33cb0f47dfca3aad26afe25cedd1d5305f
tjtln <tjtln@iastate.edu>
2021-03-31 15:40:39 -0500

    committing more stuff that i did

---
+++ Assets/Scripts/lvl2lock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lvl2lock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GlobalVariables.globalvars.level2 == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
